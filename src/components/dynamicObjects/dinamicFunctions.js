e222ea90d0810d3e903cdad344810dc8b1ce166f
Hiperiosity <anastasy_ya@mail.ru>
2024-08-27 07:29:10 +0300

    add deleted

---
+++ src/components/dynamicObjects/dinamicFunctions.js
@@ -0,0 +1,116 @@
+import * as THREE from 'three';
+
+// Basic 1
+export const handleCubeClick = (cube) => {
+
+  if (cube) {
+    cube.scale.set(Math.random() * 2 + 0.5, Math.random() * 2 + 0.5, Math.random() * 2 + 0.5);
+    cube.rotation.set(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI);
+    cube.material.color.setHex(Math.random() * 0xffffff);
+  }
+};
+
+// Basic 2
+export const createDecals = (intersection) => {
+  if (!intersection.face || !intersection.face.normal) {
+    console.log('Intersection does not have a valid face normal.');
+    return null;  // Возвращаем null, чтобы предотвратить дальнейшую обработку
+  }
+
+  // Создание декали
+  const decalMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const decalGeometry = new THREE.PlaneGeometry(1, 1);
+  const decalMesh = new THREE.Mesh(decalGeometry, decalMaterial);
+
+  // Проекция декали на поверхность
+  decalMesh.position.copy(intersection.point);
+  decalMesh.lookAt(intersection.point.clone().add(intersection.face.normal));
+  //поворачивает декаль так, чтобы она была перпендикулярна поверхности объекта, на которую она проецируется.
+
+  // Создание линии
+  const lineGeometry = new THREE.BufferGeometry();
+  const points = new Float32Array([ //создается массив из шести чисел, представляющих две точки в трехмерном пространстве: начальная и конечная точка
+    intersection.point.x, intersection.point.y, intersection.point.z,
+    intersection.point.x + intersection.face.normal.x * 10,
+    intersection.point.y + intersection.face.normal.y * 10,
+    intersection.point.z + intersection.face.normal.z * 10
+  ]);
+
+  // задает атрибуты геометрии для линии, используя точки, которые были определены выше
+  lineGeometry.setAttribute('position', new THREE.BufferAttribute(points, 3));
+  const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+  const line = new THREE.Line(lineGeometry, lineMaterial);
+
+  return line;
+};
+
+// Basic 3
+export function createLineToCentresOfGeometry(intersection) {
+
+  const { object } = intersection;
+
+  if (!intersection.face || !intersection.face.normal) {
+    console.log('Intersection does not have a valid face normal.');
+    return null;  // Возвращаем null, чтобы предотвратить дальнейшую обработку
+  }
+
+  // Создание декали
+  const decalMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const decalGeometry = new THREE.PlaneGeometry(1, 1);
+  const decalMesh = new THREE.Mesh(decalGeometry, decalMaterial);
+
+  // Проекция декали на поверхность
+  decalMesh.position.copy(intersection.point);
+  decalMesh.lookAt(intersection.point.clone().add(intersection.face.normal));
+  //поворачивает декаль так, чтобы она была перпендикулярна поверхности объекта, на которую она проецируется.
+
+  // Создание линии
+  // Создаем материал для линии
+  const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+
+  // Функция для создания линии
+  function createLine(startPoint, endPoint) {
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute([
+      startPoint.x, startPoint.y, startPoint.z,
+      endPoint.x, endPoint.y, endPoint.z
+    ], 3));
+
+    const line = new THREE.Line(geometry, lineMaterial);
+    return line;
+  }
+  return createLine(intersection.point, object.position);
+}
+
+// Basic 4
+export function changeSizeAsDistance(intersection, scene) {
+  const clickedObject = intersection.object; // Получаем объект, по которому кликнули
+
+  if (!intersection.face || !intersection.face.normal) {
+    console.log('Intersection does not have a valid face normal.');
+    return null;
+  }
+
+  // Получаем центр кликнутого объекта
+  const cubeCenter = clickedObject.position;
+
+  // Вычисляем расстояние от точки клика до центра объекта
+  const distance = intersection.point.distanceTo(cubeCenter);
+
+  // Определяем второй куб (предполагается, что он один из объектов сцены)
+  const secondCube = scene.children.find(obj => obj !== clickedObject && obj.isMesh);
+
+  if (secondCube) {
+    console.log('запущена функция изменения размера');
+    // Устанавливаем размеры второго куба равными расстоянию
+    secondCube.scale.set(distance, distance, distance);
+    // Пересчитываем boundingBox для правильного отображения (если это требуется)
+    secondCube.updateMatrixWorld();
+
+    // Возвращаем второй куб и, возможно, декаль
+    return [clickedObject, secondCube];
+  } else {
+    console.log('Second cube not found.');
+    return null;
+  }
+}
---
+++ src/components/dynamicObjects/dinamicObjects.js
@@ -0,0 +1,97 @@
+import * as THREE from 'three';
+
+// Функция для создания куба с закругленными углами
+function createRoundedBoxGeometry(width, height, depth, radius, smoothness) {
+  const shape = new THREE.Shape();
+  const eps = 0.00001;
+  const radius0 = radius - eps;
+
+  shape.absarc(eps, eps, eps, -Math.PI / 2, -Math.PI, true);
+  shape.absarc(eps, height - radius * 2, eps, Math.PI, Math.PI / 2, true);
+  shape.absarc(width - radius * 2, height - radius * 2, eps, Math.PI / 2, 0, true);
+  shape.absarc(width - radius * 2, eps, eps, 0, -Math.PI / 2, true);
+
+  const geometry = new THREE.ExtrudeGeometry(shape, {
+    depth: depth - radius * 2,
+    bevelEnabled: true,
+    bevelSegments: Math.max(smoothness, 2),
+    steps: 1,
+    bevelSize: radius0,
+    bevelThickness: radius,
+    curveSegments: Math.max(smoothness, 2),
+  });
+
+  geometry.center();
+
+  return geometry;
+}
+
+// Объекты для первых трех пунктов меню
+export function createBasic123Objects(environmentTexture) {
+  const objects = [];
+
+  // Создание геометрии с закругленными углами
+  const glassGeometry = createRoundedBoxGeometry(100, 100, 100, 10 / 3, 2); // Радиус скругления = 10, плавность = 10
+
+  const glassMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0x00ffff,
+    metalness: 0,
+    roughness: 0,
+    transmission: 1.0, // прозрачность
+    thickness: 5, // Толщина стекла
+    envMap: environmentTexture, // Текстура окружения для отражений
+    envMapIntensity: 1.0,
+    clearcoat: 1.0,
+    clearcoatRoughness: 0.1,
+  });
+
+  const glassMesh1 = new THREE.Mesh(glassGeometry, glassMaterial);
+  glassMesh1.castShadow = true;
+  glassMesh1.receiveShadow = true;
+  glassMesh1.position.x = -100;
+  glassMesh1.name = 'куб со скруглением граней';
+
+  // Обычный куб
+  const geometry = new THREE.BoxGeometry(100, 100, 100);
+
+  const material2 = new THREE.MeshPhysicalMaterial({
+    color: 0x00ffff,
+    opacity: 0.1, // Уменьшаем непрозрачность
+    transmission: 0.5, // Увеличиваем прозрачность
+    ior: 1.5,
+    roughness: 0.5, // Высокая шероховатость для матового эффекта
+    metalness: 0.0, // Отсутствие металлического блеска
+    clearcoat: 0.0, // Отсутствие прозрачного верхнего слоя
+    clearcoatRoughness: 0.2 // Шероховатость прозрачного слоя
+  });
+  const cube2 = new THREE.Mesh(geometry, material2);
+  cube2.castShadow = true;
+  cube2.receiveShadow = true;
+  cube2.position.x = 100;
+  cube2.name = 'куб';
+
+  objects.push(glassMesh1, cube2);
+
+  return objects;
+}
+
+// Объекты для 4 пункта меню
+export function createBasic4Objects() {
+  const geometry = new THREE.BoxGeometry(100, 100, 100);
+
+  const material1 = new THREE.MeshPhysicalMaterial({ color: 0x00ff00 });
+  const cube1 = new THREE.Mesh(geometry, material1);
+  cube1.name = 'cube1';
+  cube1.position.x = -150;
+
+  const material2 = new THREE.MeshPhysicalMaterial({ color: 0x0000ff });
+  const cube2 = new THREE.Mesh(geometry, material2);
+  cube2.name = 'cube2';
+  cube2.position.x = 150;
+
+  const cubes = [cube1, cube2];
+
+  return cubes;
+}
+
+
